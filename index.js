ea5b028d55e1b47088a53c18b21f01f7d56da9d2
Dakota Jones <dakota32jones@gmail.com>
2022-09-05 13:44:24 -0500

    added config file for database connection

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+const chalk = require("chalk");
+const dbConfig = require("./config/dbConfig");
+
+async function main() {
+  const dbConnection = await dbConfig();
+
+  console.log(dbConnection);
+}
+
+main();
