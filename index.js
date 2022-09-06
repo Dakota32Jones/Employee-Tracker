5fb404febc3415a4d0cf422bb7ab2be55ab0b546
Dakota Jones <dakota32jones@gmail.com>
2022-09-05 20:09:29 -0500

    added delete functions to the tables

--- index.js
+++ index.js
@@ -512,3 +512,133 @@ const updateEmployeeManager = () => {
       });
   });
 };
+
+// removing items from table
+
+// Deleting an employee
+
+const removeEmployee = () => {
+  let sql = `SELECT employee.id, employee.first_name, employee.last_name FROM employee`;
+
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    let employeeNamesArray = [];
+    response.forEach((employee) => {
+      employeeNamesArray.push(`${employee.first_name} ${employee.last_name}`);
+    });
+
+    inquirer
+      .prompt([
+        {
+          name: "chosenEmployee",
+          type: "list",
+          message: "Which employee would you like to remove?",
+          choices: employeeNamesArray,
+        },
+      ])
+      .then((answer) => {
+        let employeeId;
+
+        response.forEach((employee) => {
+          if (
+            answer.chosenEmployee ===
+            `${employee.first_name} ${employee.last_name}`
+          ) {
+            employeeId = employee.id;
+          }
+        });
+
+        let sql = `DELETE FROM employee WHERE employee.id = ?`;
+        connection.query(sql, [employeeId], (error) => {
+          if (error) throw error;
+          console.log(chalk.cyanBright.bold("=".repeat(50)));
+          console.log(chalk.magentaBright(`Employee Successfully Removed`));
+          console.log(chalk.cyanBright.bold("=".repeat(50)));
+          viewAllEmployees();
+        });
+      });
+  });
+};
+
+// Delete a role
+
+const removeRole = () => {
+  let sql = `SELECT role.id, role.title FROM role`;
+
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    let roleNamesArray = [];
+    response.forEach((role) => {
+      roleNamesArray.push(role.title);
+    });
+
+    inquirer
+      .prompt([
+        {
+          name: "chosenRole",
+          type: "list",
+          message: "Which role would you like to remove?",
+          choices: roleNamesArray,
+        },
+      ])
+      .then((answer) => {
+        let roleId;
+
+        response.forEach((role) => {
+          if (answer.chosenRole === role.title) {
+            roleId = role.id;
+          }
+        });
+
+        let sql = `DELETE FROM role WHERE role.id = ?`;
+        connection.query(sql, [roleId], (error) => {
+          if (error) throw error;
+          console.log(chalk.redBright.bold("=".repeat(50)));
+          console.log(chalk.redBright(`Role Successfully Removed`));
+          console.log(chalk.redBright.bold("=".repeat(50)));
+          viewAllRoles();
+        });
+      });
+  });
+};
+
+// Delete a department
+
+const removeDepartment = () => {
+  let sql = `SELECT department.id, department.department_name FROM department`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    let departmentNamesArray = [];
+    response.forEach((department) => {
+      departmentNamesArray.push(department.department_name);
+    });
+
+    inquirer
+      .prompt([
+        {
+          name: "chosenDept",
+          type: "list",
+          message: "Which department would you like to remove?",
+          choices: departmentNamesArray,
+        },
+      ])
+      .then((answer) => {
+        let departmentId;
+
+        response.forEach((department) => {
+          if (answer.chosenDept === department.department_name) {
+            departmentId = department.id;
+          }
+        });
+
+        let sql = `DELETE FROM department WHERE department.id = ?`;
+        connection.query(sql, [departmentId], (error) => {
+          if (error) throw error;
+          console.log(chalk.blackBright.bold("=".repeat(50)));
+          console.log(chalk.blackBright(`Department Successfully Removed`));
+          console.log(chalk.blackBright.bold("=".repeat(50)));
+          viewAllDepartments();
+        });
+      });
+  });
+};
